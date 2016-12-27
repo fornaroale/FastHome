496a296fc77c1a249009c8b6e52e0f99ef9bb767
Alessandro <aleforni97@gmail.com>
2016-12-27 17:00:25 +0100

    Aggiunto sistema Login

---
+++ app/src/main/java/jm/progettomaturita/HomeActivity.java
@@ -0,0 +1,24 @@
+package jm.progettomaturita;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        final TextView welcomeMessage = (TextView) findViewById(R.id.tvWelcomeMsg);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        String username = intent.getStringExtra("username");
+
+        String msg = "Benvenuto, " + name;
+        welcomeMessage.setText(msg);
+    }
+}
---
+++ app/src/main/java/jm/progettomaturita/LoginRequest.java
@@ -0,0 +1,29 @@
+package jm.progettomaturita;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by ale on 27/12/2016.
+ */
+
+public class LoginRequest extends StringRequest {
+
+    private static final String LOGIN_REQUEST_URL = "http://fasthome.altervista.org/login.php";
+    private Map<String, String> params;
+
+    public LoginRequest(String username, String password, Response.Listener<String> listener){
+        super(Method.POST, LOGIN_REQUEST_URL, listener, null);
+        params = new HashMap<>();
+        params.put("username", username);
+        params.put("password", password);
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return params;
+    }
+}
